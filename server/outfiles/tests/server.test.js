cb95f681ea3092b6c5854c181bb908fbd81fb7e9
Raul Reyes <rauleddie18@gmail.com>
2019-04-11 10:59:52 -0700

    Add tests for GET /todos

--- server/outfiles/tests/server.test.js
+++ server/outfiles/tests/server.test.js
@@ -7,8 +7,19 @@ var expect_1 = __importDefault(require("expect"));
 var supertest_1 = __importDefault(require("supertest"));
 var server_1 = require("../server");
 var todo_1 = require("../models/todo");
+// Dummy todo data
+var todos = [
+    {
+        text: 'First test todo'
+    },
+    {
+        text: 'Second test todo'
+    }
+];
 beforeEach(function (done) {
-    todo_1.Todo.remove({}).then(function () { return done(); });
+    todo_1.Todo.remove({}).then(function () {
+        return todo_1.Todo.insertMany(todos);
+    }).then(function () { return done(); });
 });
 describe('POST /todos', function () {
     it('should create a new todo', function (done) {
@@ -24,7 +35,7 @@ describe('POST /todos', function () {
             if (err) {
                 return done(err);
             }
-            todo_1.Todo.find().then(function (todos) {
+            todo_1.Todo.find({ text: text }).then(function (todos) {
                 expect_1.default(todos.length).toBe(1);
                 expect_1.default(todos[0].text).toBe(text);
                 done();
@@ -43,9 +54,20 @@ describe('POST /todos', function () {
                 return done(err);
             }
             todo_1.Todo.find().then(function (todos) {
-                expect_1.default(todos.length).toBe(0);
+                expect_1.default(todos.length).toBe(2);
                 done();
             }).catch(function (e) { return done(e); });
         });
     }));
+});
+describe('GET /todos', function () {
+    it('should get all todos', function (done) {
+        supertest_1.default(server_1.app)
+            .get('/todos')
+            .expect(200)
+            .expect(function (res) {
+            expect_1.default(res.body.todos.length).toBe(2);
+        })
+            .end(done);
+    });
 });
--- server/ts/tests/server.test.ts
+++ server/ts/tests/server.test.ts
@@ -4,8 +4,20 @@ import request from 'supertest';
 import {app} from '../server';
 import {Todo} from '../models/todo';
 
+// Dummy todo data
+const todos = [
+    {
+        text: 'First test todo'
+    },
+    {
+        text: 'Second test todo'
+    }
+];
+
 beforeEach((done: MochaDone) => {
-    Todo.remove({}).then( () => done());
+    Todo.remove({}).then( () => {
+        return Todo.insertMany(todos);
+    }).then( () => done());
 });
 
 describe('POST /todos', () => {
@@ -23,7 +35,7 @@ describe('POST /todos', () => {
                 if(err) {
                     return done(err);
                 }
-                Todo.find().then( (todos: any[]) => {
+                Todo.find({text}).then( (todos: any[]) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -43,9 +55,21 @@ describe('POST /todos', () => {
                     return done(err);
                 }
                 Todo.find().then((todos: any[]) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((e: Error) => done(e));
             });
     }));
+});
+
+describe('GET /todos', () => {
+    it('should get all todos', (done: MochaDone) => {
+        request(app)
+        .get('/todos')
+        .expect(200)
+        .expect((res: any) => {
+            expect(res.body.todos.length).toBe(2);
+        })
+        .end(done);
+    })
 });
