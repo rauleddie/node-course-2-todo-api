8e74ee023e7f72d96cf66d70f539ff09f742f1a2
Raul Reyes <rauleddie18@gmail.com>
2019-04-12 15:07:05 -0700

    Setup app for heroku

--- out/server/db/mongoose.js
+++ out/server/db/mongoose.js
@@ -3,5 +3,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // Load Mongoose Library
 var mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 exports.default = mongoose;
--- src/server/db/mongoose.ts
+++ src/server/db/mongoose.ts
@@ -1,5 +1,5 @@
 // Load Mongoose Library
 import mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 export default mongoose
